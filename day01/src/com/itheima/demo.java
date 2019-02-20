b429fe252b44580ad26e7e62c91f52a3123995c3
Tipsy <642853815@qq.com>
2019-02-20 21:14:08 +0800

    修改了本地文件，并添加到Git

---
+++ day01/src/com/itheima/demo.java
@@ -0,0 +1,19 @@
+package com.itheima;
+
+public class demo {
+
+    public static void main(String[] args) {
+        System.out.println("Git");
+        System.out.println("Git");
+        System.out.println("Git");
+        System.out.println("Git");
+        System.out.println("Git");
+        System.out.println("Git");
+    }
+
+    public void test1() {
+        int i = 0;
+        System.out.println(i);
+    }
+
+}
